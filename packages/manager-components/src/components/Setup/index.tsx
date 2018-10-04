33cbab8939650a2f757d0e76449e5ae1a09a610c
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-10-04 09:17:05 +0200

    Refactor setup

--- packages/manager-components/src/components/Setup/index.tsx
+++ packages/manager-components/src/components/Setup/index.tsx
@@ -29,7 +29,9 @@ export interface SetupProps {
   networkId?: string;
   touched?: any;
   values: FormValues;
-  setFieldValue: (name, value) => void;
+  onClickAccept: () => void;
+  onClickDecline: () => void;
+  signed?: boolean;
 }
 
 export const Setup: StatelessComponent<SetupProps> = ({
@@ -44,118 +46,118 @@ export const Setup: StatelessComponent<SetupProps> = ({
   networkId,
   touched,
   values,
-  setFieldValue,
-}) => {
-  const onClickAccept = async e => {
-    e.preventDefault();
-    return setFieldValue('signed', true);
-  };
+  signed,
+  onClickAccept,
+  onClickDecline,
+}) => (
+  <div className="setup">
+    <style jsx>{styles}</style>
+    <h3>Setup your fund</h3>
+    {loading && <p>Deploying your fund to the Ethereum blockchain</p>}
 
-  return (
-    <div className="setup">
-      <style jsx>{styles}</style>
-      <h3>Setup your fund</h3>
-      {loading && <p>Deploying your fund to the Ethereum blockchain</p>}
+    <Modal
+      title="Terms and Conditions"
+      isOpen={!signed}
+      PrimaryAction={Button}
+      PrimaryActionProps={{
+        children: 'Decline',
+        style: 'secondary',
+        onClick: onClickDecline,
+      }}
+      SecondaryAction={Button}
+      SecondaryActionProps={{
+        children: 'Accept',
+        onClick: onClickAccept,
+      }}
+    >
+      <TermsAndConditions />
+    </Modal>
 
-      <Modal
-        title="Terms and Conditions"
-        isOpen={!values.signed}
-        PrimaryAction={Button}
-        PrimaryActionProps={{
-          children: 'Accept',
-          onClick: onClickAccept,
-        }}
-      >
-        <TermsAndConditions />
-      </Modal>
-
-      <Form onSubmit={handleSubmit}>
-        <Input
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.name}
-          required={true}
-          name="name"
-          type="text"
-          placeholder="Fund name"
-          error={touched.name && errors.name}
-        />
-        <h4>Melon Default Configuration:</h4>
-        <p>
-          For this version, the modules that your fund will use are predefined
-          ie. you do not need to choose a module. For your record, below are the
-          predefined modules for this version.
-        </p>
-        <div className="setup__exchanges">
-          <h4>Exchange:</h4>
-          <div className="setup__exchanges-checkbox">
-            <Checkbox
-              name="exchanges"
-              value="OasisDex"
-              text="OasisDex"
-              disabled
-              defaultChecked
-            />
-          </div>
-          <div className="setup__exchanges-checkbox">
-            <Checkbox
-              name="exchanges"
-              value="0x relayers"
-              text="0x relayers"
-              disabled
-              defaultChecked
-            />
-          </div>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values.name}
+        required={true}
+        name="name"
+        type="text"
+        placeholder="Fund name"
+        error={touched.name && errors.name}
+      />
+      <h4>Melon Default Configuration:</h4>
+      <p>
+        For this version, the modules that your fund will use are predefined ie.
+        you do not need to choose a module. For your record, below are the
+        predefined modules for this version.
+      </p>
+      <div className="setup__exchanges">
+        <h4>Exchange:</h4>
+        <div className="setup__exchanges-checkbox">
+          <Checkbox
+            name="exchanges"
+            value="OasisDex"
+            text="OasisDex"
+            disabled
+            defaultChecked
+          />
         </div>
-        <div className="setup__info">
-          Pricefeed:{' '}
-          <a
-            href={`https://${
-              networkId === '42' ? 'kovan.' : ''
-            }etherscan.io/address/${config.canonicalPriceFeedAddress}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <strong>Canonical PriceFeed</strong>
-          </a>
-          <br />
-          Asset Registrar:{' '}
-          <strong>Melon {competitionName} Asset Universe</strong>
-          {isCompetition ? (
-            <div>
-              Compliance (invest/redeem):{' '}
-              <a
-                href={`https://${
-                  networkId === '42' ? 'kovan.' : ''
-                }etherscan.io/address/${config.competitionComplianceAddress}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <strong>
-                  Only {competitionName} contribution contract can invest
-                </strong>
-              </a>
-            </div>
-          ) : (
-            <div>
-              Compliance (invest/redeem):{' '}
-              <a
-                href={`https://${
-                  networkId === '42' ? 'kovan.' : ''
-                }etherscan.io/address/${config.noComplianceAddress}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <strong>No compliance - anyone can invest (in WETH)</strong>
-              </a>
-            </div>
-          )}
-          Risk Management: <strong>Disabled (all trades allowed)</strong>
+        <div className="setup__exchanges-checkbox">
+          <Checkbox
+            name="exchanges"
+            value="0x relayers"
+            text="0x relayers"
+            disabled
+            defaultChecked
+          />
         </div>
-        <Button type="submit">Create and deploy my fund!</Button>
-      </Form>
-    </div>
-  );
-};
+      </div>
+      <div className="setup__info">
+        Pricefeed:{' '}
+        <a
+          href={`https://${
+            networkId === '42' ? 'kovan.' : ''
+          }etherscan.io/address/${config.canonicalPriceFeedAddress}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <strong>Canonical PriceFeed</strong>
+        </a>
+        <br />
+        Asset Registrar: <strong>Melon {competitionName} Asset Universe</strong>
+        {isCompetition ? (
+          <div>
+            Compliance (invest/redeem):{' '}
+            <a
+              href={`https://${
+                networkId === '42' ? 'kovan.' : ''
+              }etherscan.io/address/${config.competitionComplianceAddress}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <strong>
+                Only {competitionName} contribution contract can invest
+              </strong>
+            </a>
+          </div>
+        ) : (
+          <div>
+            Compliance (invest/redeem):{' '}
+            <a
+              href={`https://${
+                networkId === '42' ? 'kovan.' : ''
+              }etherscan.io/address/${config.noComplianceAddress}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <strong>No compliance - anyone can invest (in WETH)</strong>
+            </a>
+          </div>
+        )}
+        Risk Management: <strong>Disabled (all trades allowed)</strong>
+      </div>
+      <Button type="submit">Create and deploy my fund!</Button>
+    </Form>
+  </div>
+);
 
 export default Setup;
--- packages/manager-interface/src/components/Setup/index.tsx
+++ packages/manager-interface/src/components/Setup/index.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import Setup from '@melonproject/manager-components/components/Setup/container';
 import FundMutation from './data/fund';
-import { compose } from 'recompose';
+import { compose, withHandlers, withState } from 'recompose';
+import Router from 'next/router';
+
+const withSignedState = withState('signed', 'setSigned', false);
+
+const withSetupHandlers = withHandlers({
+  onClickDecline: props => e => {
+    Router.replace({
+      pathname: '/wallet',
+    });
+  },
+  onClickAccept: props => e => {
+    props.setSigned(true);
+  },
+});
 
 const withSetup = BaseComponent => baseProps => (
   <FundMutation>
     {createFund => (
       <BaseComponent
+        onClickDecline={baseProps.onClickDecline}
+        signed={baseProps.signed}
+        onClickAccept={baseProps.onClickAccept}
         config={baseProps.config}
         onSubmit={values =>
           createFund({
             variables: {
               name: values.name,
-              signed: values.signed,
-              privateKey: baseProps.privateKey
+              signed: baseProps.signed,
+              privateKey: baseProps.privateKey,
             },
           })
         }
@@ -23,5 +40,7 @@ const withSetup = BaseComponent => baseProps => (
 );
 
 export default compose(
+  withSignedState,
+  withSetupHandlers,
   withSetup,
 )(Setup);
