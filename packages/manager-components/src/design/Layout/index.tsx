4795b64fbd1e88ada3b11ee1151f7ba3d824e533
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-10-10 10:27:55 +0200

    Refactor layout

--- packages/manager-components/src/design/Layout/index.tsx
+++ packages/manager-components/src/design/Layout/index.tsx
@@ -2,7 +2,7 @@ import React, { StatelessComponent } from 'react';
 
 import styles from './styles.css';
 
-const Layout: StatelessComponent<LayoutProps> = ({ children }) => {
+const Layout: StatelessComponent = ({ children }) => {
   return (
     <div className="layout">
       <style jsx>{styles}</style>
--- packages/manager-components/src/design/Layout/story.tsx
+++ packages/manager-components/src/design/Layout/story.tsx
@@ -1,29 +1,5 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import Layouts from './index';
-import Header from '~/components/Header';
-import Footer from '~/components/Footer';
-import Title from '~/blocks/Title/index';
+import Layout from './index';
 
-storiesOf('Design|Layouts', module).add('Default', () => (
-  <Layouts
-    Header={Header}
-    HeaderProps={{
-      network: '',
-      message: '',
-      address: '',
-      balances: {
-        eth: '',
-        mln: '',
-      },
-    }}
-    Footer={Footer}
-    FooterProps
-    Title={Title}
-    TitleProps={{
-      title: 'sdfsdf',
-      icon: 'icons_wallet',
-      text: 'Hello World'
-    }}
-  />
-));
+storiesOf('Design|Layouts', module).add('Default', () => <Layout />);
