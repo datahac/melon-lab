0892289fa95c0476860d8ae376dde680933b92d9
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-11-06 14:42:57 +0100

    Fix media queries

--- packages/manager-interface/postcss.config.js
+++ packages/manager-interface/postcss.config.js
@@ -10,6 +10,7 @@ module.exports = {
       browsers: '> 0.5%, last 2 versions, Firefox ESR, not dead',
       features: {
         'nesting-rules': true,
+        'custom-media-queries': true,
         'color-mod-function': {
           unresolved: 'warn',
         },
