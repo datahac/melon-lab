54d288c6c193ccfb327f926c01c773f460d5a7a5
Sebastian Siemssen <fubhy@fubhy.com>
2018-12-03 13:19:12 +0100

    Fix timeout.

--- src/schema/utils/pollPriceFeed.ts
+++ src/schema/utils/pollPriceFeed.ts
@@ -1,20 +1,23 @@
 import * as Rx from 'rxjs';
-import { timeout, retryWhen, delay, expand, concatMap } from 'rxjs/operators';
+// import { timeout, retryWhen, delay, expand, concatMap } from 'rxjs/operators';
 
-const requestPriceFeed = environment => {
-  // TODO: Implement this properly.
-  const recentPrice = [null];
-  return Rx.from(recentPrice).pipe(
-    timeout(10000),
-    retryWhen(errors => errors.pipe(delay(1000))),
-  );
-};
+// const requestPriceFeed = environment => {
+//   return Rx.from(recentPrice).pipe(
+//     timeout(10000),
+//     retryWhen(errors => errors.pipe(delay(1000))),
+//   );
+// };
 
-const pollPriceFeed = environment => {
-  const doPoll = () =>
-    Rx.timer(5000).pipe(concatMap(() => requestPriceFeed(environment)));
+// const pollPriceFeed = environment => {
+//   const doPoll = () =>
+//     Rx.timer(5000).pipe(concatMap(() => requestPriceFeed(environment)));
 
-  return requestPriceFeed(environment).pipe(expand(doPoll));
+//   return requestPriceFeed(environment).pipe(expand(doPoll));
+// };
+
+// TODO: Implement this properly.
+const pollPriceFeed = () => {
+  return Rx.of(false);
 };
 
 export default pollPriceFeed;
--- src/schema/utils/pollRanking.ts
+++ src/schema/utils/pollRanking.ts
@@ -1,20 +1,23 @@
 import * as Rx from 'rxjs';
-import { timeout, retryWhen, delay, concatMap, expand } from 'rxjs/operators';
+// import { timeout, retryWhen, delay, concatMap, expand } from 'rxjs/operators';
 
-const requestRanking = environment => {
-  // TODO: Implement this properly.
-  const ranking = [];
-  return Rx.from(ranking).pipe(
-    timeout(10000),
-    retryWhen(errors => errors.pipe(delay(1000))),
-  );
-};
+// const requestRanking = environment => {
+//   return Rx.from(ranking).pipe(
+//     timeout(10000),
+//     retryWhen(errors => errors.pipe(delay(1000))),
+//   );
+// };
 
-const pollRanking = environment => {
-  const doPoll = () =>
-    Rx.timer(60000).pipe(concatMap(() => requestRanking(environment)));
+// const pollRanking = environment => {
+//   const doPoll = () =>
+//     Rx.timer(60000).pipe(concatMap(() => requestRanking(environment)));
 
-  return requestRanking(environment).pipe(expand(doPoll));
+//   return requestRanking(environment).pipe(expand(doPoll));
+// };
+
+// TODO: Implement this properly.
+const pollRanking = () => {
+  return Rx.of([]);
 };
 
 export default pollRanking;
