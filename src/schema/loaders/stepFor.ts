06811e0eb1e99ec347c435cb3ba7139caf9b7bcc
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-12-19 15:09:15 +0100

    Fix getStepFor error

--- src/schema/loaders/stepFor.ts
+++ src/schema/loaders/stepFor.ts
@@ -1,8 +1,7 @@
 import * as R from 'ramda';
-import { getStepFor } from '@melonproject/protocol';
 
 function stepFor(environment, managerAddress) {
-  return getStepFor(environment, managerAddress);
+  return null;
 }
 
 export default R.curryN(2, stepFor);
