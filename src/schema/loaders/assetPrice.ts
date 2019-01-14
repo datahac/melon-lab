b16d3216267402581fb5f93f3bf506ff420de125
Simon Emanuel Schmid <simon@schmid.io>
2019-01-14 22:50:21 +0100

    Fix some typescript stuff

--- src/schema/loaders/assetPrice.ts
+++ src/schema/loaders/assetPrice.ts
@@ -1,5 +1,6 @@
-import * as R from 'ramda';
 import * as Tm from '@melonproject/token-math';
+import * as R from 'ramda';
+
 import { getPrice } from '@melonproject/protocol';
 
 export default R.curryN(2, (environment, token: Tm.token.TokenInterface) => {
