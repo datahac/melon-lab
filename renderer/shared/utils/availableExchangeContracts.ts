ad3bb17c2a9c22e69f366fe986b5e84c03186182
Simon Emanuel Schmid <simon@schmid.io>
2019-01-27 10:57:18 +0100

    Only show the implemented exchanges for now

--- renderer/shared/utils/availableExchangeContracts.ts
+++ renderer/shared/utils/availableExchangeContracts.ts
@@ -3,18 +3,18 @@ const availableExchangeContracts = [
     value: 'MATCHING_MARKET',
     text: 'OasisDex',
   },
-  {
-    value: 'ZERO_EX_EXCHANGE',
-    text: 'Radar Relay',
-  },
+  // {
+  //   value: 'ZERO_EX_EXCHANGE',
+  //   text: 'Radar Relay',
+  // },
   {
     value: 'KYBER_NETWORK',
     text: 'Kyber Network',
   },
-  {
-    value: 'ETHFINEX',
-    text: 'ETH Finex',
-  },
+  // {
+  //   value: 'ETHFINEX',
+  //   text: 'ETH Finex',
+  // },
 ];
 
 export default availableExchangeContracts;
