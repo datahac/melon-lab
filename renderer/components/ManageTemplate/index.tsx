c48d27cd8b0a72797c4a2ff53778984411f4bb6b
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-01-31 16:39:10 +0100

    Add hooks to manage template

--- renderer/components/ManageTemplate/index.tsx
+++ renderer/components/ManageTemplate/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Composer from 'react-composer';
 import * as R from 'ramda';
 import * as Tm from '@melonproject/token-math';
@@ -59,157 +59,148 @@ const Container = ({ address, children }) => {
   );
 };
 
-export default class ManageTemplateContainer extends React.Component {
-  state = {
-    order: {
-      // Order ID is set when clicking on order in orderbook
-      id: null,
-      exchange: 'OASIS_DEX',
-      price: null,
-      quantity: null,
-      strategy: 'Market',
-      total: null,
-      type: 'Buy',
-      signedOrder: null,
-    },
+export default ({ address, quoteAsset, baseAsset }) => {
+  const [selectedOrder, setSelectedOrder] = useState({
+    // Order ID is set when clicking on order in orderbook
+    id: null,
+    exchange: 'OASIS_DEX',
+    price: null,
+    quantity: null,
+    strategy: 'Market',
+    total: null,
+    type: 'Buy',
+    signedOrder: null,
+  });
+
+  const setOrder = order => {
+    const { exchange, trade, metadata, type } = order;
+
+    if (exchange === 'OASIS_DEX') {
+      setSelectedOrder({
+        id: metadata && metadata.id,
+        exchange: 'OASIS_DEX',
+        price: trade,
+        quantity: trade && trade.base,
+        strategy: 'Market',
+        total: trade && trade.quote,
+        type: bidAskSellBuyMap[type],
+        signedOrder: null,
+      });
+    } else if (exchange === 'RADAR_RELAY') {
+      setSelectedOrder({
+        id: order.id,
+        exchange: 'RADAR_RELAY',
+        price: trade,
+        quantity: trade && trade.base,
+        strategy: 'Market',
+        total: trade && trade.quote,
+        type: bidAskSellBuyMap[type],
+        signedOrder: metadata,
+      });
+    } else {
+      setSelectedOrder({
+        exchange,
+        strategy: 'Market',
+        type: bidAskSellBuyMap[type],
+        id: null,
+        price: null,
+        total: null,
+        signedOrder: null,
+        quantity: null,
+      });
+    }
   };
 
-  render() {
-    const { address, quoteAsset, baseAsset } = this.props;
-
-    const setOrder = order => {
-      const { exchange, trade, metadata, type } = order;
-
-      if (exchange === 'OASIS_DEX') {
-        this.setState({
-          order: {
-            id: metadata && metadata.id,
-            exchange: 'OASIS_DEX',
-            price: trade,
-            quantity: trade && trade.base,
-            strategy: 'Market',
-            total: trade && trade.quote,
-            type: bidAskSellBuyMap[type],
-          },
-        });
-      } else if (exchange === 'RADAR_RELAY') {
-        this.setState({
-          order: {
-            id: order.id,
-            exchange: 'RADAR_RELAY',
-            price: trade,
-            quantity: trade && trade.base,
-            strategy: 'Market',
-            total: trade && trade.quote,
-            type: bidAskSellBuyMap[type],
-            signedOrder: metadata,
-          },
-        });
-      } else {
-        this.setState({
-          order: {
-            exchange,
-            strategy: 'Market',
-            type: bidAskSellBuyMap[type],
-            id: null,
-            price: null,
-            total: null,
-          },
-        });
-      }
-    };
-
-    return (
-      <Container address={address}>
-        {([
-          account,
-          balances,
-          network,
-          capabibility,
-          configuration,
-          managerProps,
+  return (
+    <Container address={address}>
+      {([
+        account,
+        balances,
+        network,
+        capabibility,
+        configuration,
+        managerProps,
+        holdingsProps,
+        fundProps,
+      ]) => {
+        const holdingsData = R.pathOr([], ['data', 'fund', 'holdings'])(
           holdingsProps,
-          fundProps,
-        ]) => {
-          const holdingsData = R.pathOr([], ['data', 'fund', 'holdings'])(
-            holdingsProps,
-          );
-          const fundData = R.pathOr({}, ['data', 'fund'])(fundProps);
-          const totalFunds = R.pathOr(0, ['data', 'totalFunds'])(fundProps);
-          const isManager =
-            !!managerProps.fund && isSameAddress(managerProps.fund, address);
-          return (
-            <Template
-              HeaderProps={{
-                ethBalance: balances && balances.eth,
-                canInvest: capabibility && capabibility.canInvest,
-                canInteract: capabibility && capabibility.canInteract,
-                canonicalPriceFeedAddress:
-                  configuration && configuration.melonContracts.priceSource,
-                network: network && network.network,
-                currentBlock: network && network.currentBlock,
-                blockOverdue: network && network.blockOverdue,
-                nodeSynced: network && network.nodeSynced,
-                priceFeedUp: network && network.priceFeedUp,
-                address: account,
-              }}
-              FundHeadlineProps={{
-                ...fundData,
-                totalFunds,
-                address,
-                quoteAsset,
-                loading: fundProps.loading,
-              }}
-              FactSheet={FactSheet}
-              FactSheetProps={{
-                fund: fundData,
-                address,
-                loading: fundProps.loading,
-                isManager,
-              }}
-              Holdings={Holdings}
-              HoldingsProps={{
-                address,
-                quoteAsset,
-                baseAsset,
-                holdings: holdingsData,
-                loading: fundProps.loading || holdingsProps.loading,
-                nav: fundData && fundData.nav,
-              }}
-              OrderForm={OrderForm}
-              OrderFormProps={{
-                address,
-                quoteAsset,
-                baseAsset,
-                setOrder,
-                holdings: holdingsData,
-                formValues: this.state.order,
-                key: baseAsset,
-              }}
-              OrderBook={OrderBook}
-              OrderBookProps={{
-                quoteAsset,
-                baseAsset,
-                isManager,
-                setOrder,
-              }}
-              OpenOrders={OpenOrdersContainer}
-              OpenOrdersProps={{
-                address,
-                isManager,
-                canInteract: capabibility && capabibility.canInteract,
+        );
+        const fundData = R.pathOr({}, ['data', 'fund'])(fundProps);
+        const totalFunds = R.pathOr(0, ['data', 'totalFunds'])(fundProps);
+        const isManager =
+          !!managerProps.fund && isSameAddress(managerProps.fund, address);
+        return (
+          <Template
+            HeaderProps={{
+              ethBalance: balances && balances.eth,
+              canInvest: capabibility && capabibility.canInvest,
+              canInteract: capabibility && capabibility.canInteract,
+              canonicalPriceFeedAddress:
+                configuration && configuration.melonContracts.priceSource,
+              network: network && network.network,
+              currentBlock: network && network.currentBlock,
+              blockOverdue: network && network.blockOverdue,
+              nodeSynced: network && network.nodeSynced,
+              priceFeedUp: network && network.priceFeedUp,
+              address: account,
+            }}
+            FundHeadlineProps={{
+              ...fundData,
+              totalFunds,
+              address,
+              quoteAsset,
+              loading: fundProps.loading,
+            }}
+            FactSheet={FactSheet}
+            FactSheetProps={{
+              fund: fundData,
+              address,
+              loading: fundProps.loading,
+              isManager,
+            }}
+            Holdings={Holdings}
+            HoldingsProps={{
+              address,
+              quoteAsset,
+              baseAsset,
+              holdings: holdingsData,
+              loading: fundProps.loading || holdingsProps.loading,
+              nav: fundData && fundData.nav,
+            }}
+            OrderForm={OrderForm}
+            OrderFormProps={{
+              address,
+              quoteAsset,
+              baseAsset,
+              setOrder,
+              holdings: holdingsData,
+              formValues: selectedOrder,
+              key: baseAsset,
+            }}
+            OrderBook={OrderBook}
+            OrderBookProps={{
+              quoteAsset,
+              baseAsset,
+              isManager,
+              setOrder,
+            }}
+            OpenOrders={OpenOrdersContainer}
+            OpenOrdersProps={{
+              address,
+              isManager,
+              canInteract: capabibility && capabibility.canInteract,
+              // TODO: Re-add this.
+            }}
+            RecentTrades={() => null}
+            RecentTradesProps={
+              {
                 // TODO: Re-add this.
-              }}
-              RecentTrades={() => null}
-              RecentTradesProps={
-                {
-                  // TODO: Re-add this.
-                }
               }
-            />
-          );
-        }}
-      </Container>
-    );
-  }
-}
+            }
+          />
+        );
+      }}
+    </Container>
+  );
+};
