c7fd08d1d30871356ae31225a9cc71256709c216
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-01-31 16:57:47 +0100

    Add orderbook subscription to manage template

--- renderer/components/ManageTemplate/index.tsx
+++ renderer/components/ManageTemplate/index.tsx
@@ -19,6 +19,19 @@ import HoldingsQuery from './data/holdings';
 import isSameAddress from '~/shared/utils/isSameAddress';
 import OpenOrdersContainer from '../OpenOrders';
 
+import { Subscription } from 'react-apollo';
+import { OrdersSubscription as query } from '~/queries/OrdersSubscription.gql';
+import {
+  isBidOrder,
+  isAskOrder,
+  sortOrders,
+  reduceOrderVolumes,
+  reduceOrderEvents,
+} from '@melonproject/exchange-aggregator';
+import availableExchanges from '~/shared/utils/availableExchanges';
+import { useEventCallback } from 'rxjs-hooks';
+import { scan, map, combineLatest } from 'rxjs/operators';
+
 // A bid-order on the orderbook resolves to a sell from the fund (The order maker wants to buy something, so we sell em)
 const bidAskSellBuyMap = {
   BID: 'Sell',
@@ -38,7 +51,83 @@ export interface SetOrder {
   type: string;
 }
 
-const Container = ({ address, children }) => {
+const useExchangeSelector = availableExchangesKeys => {
+  const [current, set] = useState(availableExchangesKeys);
+
+  const setFromEvent = event => {
+    const value = event.target.value;
+
+    if (value === 'ALL') {
+      const unselect = current.length === availableExchangesKeys.length;
+      return set(unselect ? [] : availableExchangesKeys);
+    }
+
+    if (!current.includes(value)) {
+      return set([...current, value]);
+    }
+
+    const index = current.indexOf(value);
+    return set([].concat(current.slice(0, index), current.slice(index + 1)));
+  };
+
+  return [current, setFromEvent];
+};
+
+const OrdersSubscription = ({
+  eventCallback,
+  exchange,
+  baseAsset,
+  quoteAsset,
+  children,
+}) => (
+  <Subscription
+    ssr={false}
+    subscription={query}
+    variables={{
+      exchange,
+      base: baseAsset,
+      quote: quoteAsset,
+    }}
+    onSubscriptionData={({ subscriptionData: { data } }) => {
+      eventCallback(data.orders);
+    }}
+  >
+    {children}
+  </Subscription>
+);
+
+const AggregatedOrders = ({
+  eventCallback,
+  baseAsset,
+  quoteAsset,
+  exchanges,
+  children,
+}) => (
+  <Composer
+    components={exchanges.map(exchange => (
+      <OrdersSubscription
+        eventCallback={eventCallback}
+        exchange={exchange}
+        quoteAsset={quoteAsset}
+        baseAsset={baseAsset}
+      />
+    ))}
+  >
+    {orderResponses => {
+      const loading = !!orderResponses.find(R.propEq('loading', true));
+      return children({ loading });
+    }}
+  </Composer>
+);
+
+const Container = ({
+  address,
+  quoteAsset,
+  baseAsset,
+  allExchangesKeys,
+  eventCallback,
+  children,
+}) => {
   return (
     <Composer
       components={[
@@ -52,6 +141,12 @@ const Container = ({ address, children }) => {
         ({ results: [account], render }) => (
           <FundQuery address={address} account={account} children={render} />
         ),
+        <AggregatedOrders
+          quoteAsset={quoteAsset}
+          baseAsset={baseAsset}
+          exchanges={allExchangesKeys}
+          eventCallback={eventCallback}
+        />,
       ]}
     >
       {children}
@@ -71,6 +166,45 @@ export default ({ address, quoteAsset, baseAsset }) => {
     type: 'Buy',
     signedOrder: null,
   });
+  const allExchanges = Object.entries(availableExchanges);
+  const allExchangesKeys = Object.keys(availableExchanges);
+  const [selectedExchanges, updateExchanges] = useExchangeSelector(
+    allExchangesKeys,
+  );
+
+  const [eventCallback, [asks, bids]] = useEventCallback(
+    (events$, inputs$, _) =>
+      (events$ as any).pipe(
+        scan(
+          (carry, events) => (events || []).reduce(reduceOrderEvents, carry),
+          [],
+        ),
+        combineLatest(inputs$, (orders, [exchanges]) => {
+          const filter = R.compose(
+            R.includes(R.__, exchanges),
+            R.prop('exchange'),
+          );
+
+          return orders.filter(filter);
+        }),
+        map(orders => {
+          const asks = orders
+            .filter(isAskOrder)
+            .sort(sortOrders)
+            .reverse()
+            .reduce(reduceOrderVolumes, []);
+
+          const bids = orders
+            .filter(isBidOrder)
+            .sort(sortOrders)
+            .reduce(reduceOrderVolumes, []);
+
+          return [asks, bids];
+        }),
+      ),
+    [[], []],
+    [selectedExchanges],
+  );
 
   const setOrder = order => {
     const { exchange, trade, metadata, type } = order;
@@ -112,7 +246,13 @@ export default ({ address, quoteAsset, baseAsset }) => {
   };
 
   return (
-    <Container address={address}>
+    <Container
+      address={address}
+      baseAsset={baseAsset}
+      quoteAsset={quoteAsset}
+      allExchangesKeys={allExchangesKeys}
+      eventCallback={eventCallback}
+    >
       {([
         account,
         balances,
@@ -122,6 +262,7 @@ export default ({ address, quoteAsset, baseAsset }) => {
         managerProps,
         holdingsProps,
         fundProps,
+        orderbookProps,
       ]) => {
         const holdingsData = R.pathOr([], ['data', 'fund', 'holdings'])(
           holdingsProps,
@@ -184,6 +325,12 @@ export default ({ address, quoteAsset, baseAsset }) => {
               baseAsset,
               isManager,
               setOrder,
+              allExchanges,
+              updateExchanges,
+              selectedExchanges,
+              asks,
+              bids,
+              loading: orderbookProps.loading,
             }}
             OpenOrders={OpenOrdersContainer}
             OpenOrdersProps={{
--- renderer/components/OrderBook/index.tsx
+++ renderer/components/OrderBook/index.tsx
@@ -1,149 +1,26 @@
-import React, { useState } from 'react';
-import { useEventCallback } from 'rxjs-hooks';
-import { scan, map, combineLatest } from 'rxjs/operators';
-import * as R from 'ramda';
+import React from 'react';
 import OrderBook from '~/components/OrderBook';
-import Composer from 'react-composer';
-import { Subscription } from 'react-apollo';
-import {
-  isBidOrder,
-  isAskOrder,
-  sortOrders,
-  reduceOrderVolumes,
-  reduceOrderEvents,
-} from '@melonproject/exchange-aggregator';
-import availableExchanges from '~/shared/utils/availableExchanges';
-import { OrdersSubscription as query } from '~/queries/OrdersSubscription.gql';
 
-const OrdersSubscription = ({
-  eventCallback,
-  exchange,
-  baseAsset,
-  quoteAsset,
-  children,
-}) => (
-  <Subscription
-    ssr={false}
-    subscription={query}
-    variables={{
-      exchange,
-      base: baseAsset,
-      quote: quoteAsset,
-    }}
-    onSubscriptionData={({ subscriptionData: { data } }) => {
-      eventCallback(data.orders);
-    }}
-  >
-    {children}
-  </Subscription>
-);
-
-const AggregatedOrders = ({
-  eventCallback,
-  baseAsset,
-  quoteAsset,
-  exchanges,
-  children,
-}) => (
-  <Composer
-    components={exchanges.map(exchange => (
-      <OrdersSubscription
-        eventCallback={eventCallback}
-        exchange={exchange}
-        quoteAsset={quoteAsset}
-        baseAsset={baseAsset}
-      />
-    ))}
-  >
-    {orderResponses => {
-      const loading = !!orderResponses.find(R.propEq('loading', true));
-      return children({ loading });
-    }}
-  </Composer>
-);
-
-const useExchangeSelector = availableExchangesKeys => {
-  const [current, set] = useState(availableExchangesKeys);
-
-  const setFromEvent = event => {
-    const value = event.target.value;
-
-    if (value === 'ALL') {
-      const unselect = current.length === availableExchangesKeys.length;
-      return set(unselect ? [] : availableExchangesKeys);
-    }
-
-    if (!current.includes(value)) {
-      return set([...current, value]);
-    }
-
-    const index = current.indexOf(value);
-    return set([].concat(current.slice(0, index), current.slice(index + 1)));
-  };
-
-  return [current, setFromEvent];
-};
-
-export default ({ baseAsset, quoteAsset, isManager, setOrder }) => {
-  const allExchanges = Object.entries(availableExchanges);
-  const allExchangesKeys = Object.keys(availableExchanges);
-  const [selectedExchanges, updateExchanges] = useExchangeSelector(
-    allExchangesKeys,
-  );
-
-  const [eventCallback, [asks, bids]] = useEventCallback(
-    (events$, inputs$, _) =>
-      (events$ as any).pipe(
-        scan(
-          (carry, events) => (events || []).reduce(reduceOrderEvents, carry),
-          [],
-        ),
-        combineLatest(inputs$, (orders, [exchanges]) => {
-          const filter = R.compose(
-            R.includes(R.__, exchanges),
-            R.prop('exchange'),
-          );
-
-          return orders.filter(filter);
-        }),
-        map(orders => {
-          const asks = orders
-            .filter(isAskOrder)
-            .sort(sortOrders)
-            .reverse()
-            .reduce(reduceOrderVolumes, []);
-
-          const bids = orders
-            .filter(isBidOrder)
-            .sort(sortOrders)
-            .reduce(reduceOrderVolumes, []);
-
-          return [asks, bids];
-        }),
-      ),
-    [[], []],
-    [selectedExchanges],
-  );
-
+export default ({
+  asks,
+  bids,
+  loading,
+  isManager,
+  setOrder,
+  allExchanges,
+  updateExchanges,
+  selectedExchanges,
+}) => {
   return (
-    <AggregatedOrders
-      quoteAsset={quoteAsset}
-      baseAsset={baseAsset}
-      exchanges={allExchangesKeys}
-      eventCallback={eventCallback}
-    >
-      {({ loading }) => (
-        <OrderBook
-          loading={loading}
-          bids={bids}
-          asks={asks}
-          availableExchanges={allExchanges}
-          setExchange={updateExchanges}
-          selectedExchanges={selectedExchanges}
-          isManager={isManager}
-          setOrder={setOrder}
-        />
-      )}
-    </AggregatedOrders>
+    <OrderBook
+      loading={loading}
+      bids={bids}
+      asks={asks}
+      availableExchanges={allExchanges}
+      setExchange={updateExchanges}
+      selectedExchanges={selectedExchanges}
+      isManager={isManager}
+      setOrder={setOrder}
+    />
   );
 };
