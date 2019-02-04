6ce04e6ff0c240230a3e46fe54c6ecdbcd8b7361
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-02-04 18:46:49 +0100

    Check if valid fund & user

--- renderer/pages/invest.tsx
+++ renderer/pages/invest.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import DefaultTemplate from '+/components/DefaultTemplate';
 import Invest from '+/components/Invest';
+import checkValidFund from '~/shared/utils/checkValidFund';
+import checkIsLoggedIn from '~/shared/utils/checkIsLoggedIn';
+import Error from '~/error';
+import isError from '~/shared/utils/isError';
 
 export default class InvestPage extends React.Component {
   static async getInitialProps(context) {
     const parameters = (context.req && context.req.query) || context.query;
+    const isLoggedIn = await checkIsLoggedIn(context.apolloClient);
+    const isValidFund = await checkValidFund(
+      context.apolloClient,
+      parameters.address,
+    );
 
-    // TODO: Check if the given fund exists.
+    if (!isLoggedIn) {
+      return { statusCode: 403 };
+    }
+
+    if (!isValidFund) {
+      return { statusCode: 404 };
+    }
+
     return {
       address: parameters.address,
     };
   }
 
   render() {
+    const { statusCode, ...props } = this.props;
+    if (statusCode && isError(statusCode)) {
+      return <Error statusCode={statusCode} />;
+    }
+
     return (
       <DefaultTemplate title="Invest">
         <Invest address={this.props.address} />
---
+++ renderer/shared/utils/checkValidFund.ts
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag';
+import * as R from 'ramda';
+
+export default (apolloClient, address) =>
+  apolloClient
+    .query({
+      variables: {
+        address,
+      },
+      query: gql`
+        query FundQuery($address: String!) {
+          fund(address: $address) {
+            id
+          }
+        }
+      `,
+    })
+    .then(async ({ data }) => {
+      if (R.path(['fund', 'address'], data)) {
+        return true;
+      }
+      return false;
+    })
+    .catch(() => {
+      // Fail gracefully
+      return false;
+    });
