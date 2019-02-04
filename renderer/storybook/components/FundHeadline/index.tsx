7aec8691df675f04b22d89da9993efdecc5b6f31
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-02-04 09:26:42 +0100

    Remove twitter link

--- renderer/storybook/components/FundHeadline/index.tsx
+++ renderer/storybook/components/FundHeadline/index.tsx
@@ -14,8 +14,6 @@ export interface FundHeadlineProps {
   rank?: string;
   totalFunds?: string;
   quoteAsset?: string;
-  owner?: string;
-  account?: string;
   address?: string;
   track?: string;
   loading?: boolean;
@@ -28,44 +26,11 @@ const FundHeadline: StatelessComponent<FundHeadlineProps> = ({
   gav,
   rank,
   totalFunds,
-  owner,
-  account,
   address,
   track,
   loading,
   decimals = 4,
 }) => {
-  const isOwner = owner === account;
-  const buildTwitterUrl = () => {
-    const text = isOwner
-      ? track !== 'live'
-        ? `My #MelonFund "${name}" has a share price currently of ${sharePrice &&
-            displayPrice(sharePrice)}. Have a look:`
-        : `Check out my on-chain decentralized hedge fund "${name}". ` +
-          `It currently has a share price of ${sharePrice &&
-            displayPrice(sharePrice)}. Have a look:`
-      : track !== 'live'
-      ? `The #MelonFund "${name}" has a share price currently of ${sharePrice &&
-          displayPrice(sharePrice)}. Have a look:`
-      : `Check out this on-chain decentralized hedge fund "${name}". ` +
-        `It currently has a share price of ${sharePrice &&
-          displayPrice(sharePrice)}. Have a look:`;
-
-    const url =
-      track === 'live'
-        ? `https://olympiad.melon.fund/${address}`
-        : `https://melon.fund/${address}`;
-    const hashtags = 'TechnologyRegulatedFunds,Melon,MelonFund';
-    const via = 'melonport';
-    const related = 'melonport';
-
-    return `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-      text,
-    )}&url=${encodeURIComponent(url)}&hashtags=${encodeURIComponent(
-      hashtags,
-    )}&via=${encodeURIComponent(via)}&related=${encodeURIComponent(related)}`;
-  };
-
   const fundUrl =
     track === 'live'
       ? `https://etherscan.io/address/${address}`
@@ -81,19 +46,7 @@ const FundHeadline: StatelessComponent<FundHeadlineProps> = ({
       ) : (
         <Fragment>
           <div className="fund-headline__headline">
-            <h1 className="fund-headline__title">
-              <span className="fund-headline__name">{name}</span>
-              <span className="fund-headline__twitter">
-                <a
-                  href={buildTwitterUrl()}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="fund-headline__twitter-link"
-                >
-                  <Icons width="14px" height="14px" name="twitter" />
-                </a>
-              </span>
-            </h1>
+            <h1 className="fund-headline__title">{name}</h1>
             <div className="fund-headline__address">
               <a href={fundUrl} target="_blank" rel="noopener noreferrer">
                 {address}
