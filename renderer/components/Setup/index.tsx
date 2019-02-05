08aa3ff5540cd0da4567e84f0ce2d046b5683610
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-02-05 16:23:32 +0100

    Refactor setup

--- renderer/components/Setup/index.tsx
+++ renderer/components/Setup/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { withRouter } from 'next/router';
 import { compose } from 'recompose';
 import Wizard from '~/components/Wizard';
@@ -23,6 +23,40 @@ import * as R from 'ramda';
 import availablePolicies from '~/shared/utils/availablePolicies';
 import availableExchangeContracts from '~/shared/utils/availableExchangeContracts';
 import { TokensQuery } from './data/tokens';
+
+const steps = [
+  {
+    key: 'fund',
+    name: 'Fund',
+    validateFields: ['name', 'exchanges'],
+  },
+  {
+    key: 'fee-structure',
+    name: 'Fee structure',
+    validateFields: ['fees.performanceFee', 'fees.managementFee'],
+  },
+  {
+    key: 'risk-profile',
+    name: 'Risk Profile',
+    validateFields: [
+      'policies.priceTolerance',
+      'policies.maxPositions',
+      'policies.maxConcentration',
+      'policies.assetWhitelist',
+      'policies.assetWhitelist',
+      'policies.assetWhitelist',
+    ],
+  },
+  {
+    key: 'terms-conditions',
+    name: 'Terms & Conditions',
+    validateFields: ['terms'],
+  },
+  {
+    key: 'overview',
+    name: 'Overview',
+  },
+];
 
 const SetupFormContainer = withForm(props => {
   const tokens =
@@ -99,126 +133,71 @@ const SetupFormContainer = withForm(props => {
   );
 });
 
-class Setup extends React.Component {
-  state = {
-    values: undefined,
-    page: 0,
-    showModal: false,
-    steps: [
-      {
-        key: 'fund',
-        name: 'Fund',
-        validateFields: ['name', 'exchanges'],
-      },
-      {
-        key: 'fee-structure',
-        name: 'Fee structure',
-        validateFields: ['fees.performanceFee', 'fees.managementFee'],
-      },
-      {
-        key: 'risk-profile',
-        name: 'Risk Profile',
-        validateFields: [
-          'policies.priceTolerance',
-          'policies.maxPositions',
-          'policies.maxConcentration',
-          'policies.assetWhitelist',
-          'policies.assetWhitelist',
-          'policies.assetWhitelist',
-        ],
-      },
-      {
-        key: 'terms-conditions',
-        name: 'Terms & Conditions',
-        validateFields: ['terms'],
-      },
-      {
-        key: 'overview',
-        name: 'Overview',
-      },
-    ],
-  };
+const Setup = ({ ...props }) => {
+  const [fundValues, setFundValues] = useState(null);
+  const [page, setPage] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
-  setFundValues = values => {
-    this.setState({
-      values,
-    });
-  };
+  return (
+    <Composer
+      components={[
+        <AccountConsumer />,
+        <ConfigurationConsumer />,
+        <FundManagerConsumer />,
+        <SetupConsumer />,
+      ]}
+    >
+      {([account, configuration, manager, setup]) => (
+        <Fragment>
+          <SetupTransactions
+            progress={
+              (setup.setupBegin && !!fundValues) ||
+              setup.setupInProgress ||
+              setup.setupComplete
+            }
+            values={fundValues}
+            update={manager.update}
+            updateSetup={setup.update}
+            routes={manager.routes}
+            fund={manager.fund}
+            setup={setup}
+          />
 
-  setShowModal = showModal => {
-    this.setState({
-      showModal,
-    });
-  };
+          <PoliciesTransactions
+            progress={
+              !R.isEmpty(R.path(['policies'], fundValues)) &&
+              setup.isComplete &&
+              !!manager.fund
+            }
+            values={fundValues}
+            fund={manager.fund}
+          />
 
-  setPage = page => {
-    this.setState({
-      page,
-    });
-  };
-
-  render() {
-    return (
-      <Composer
-        components={[
-          <AccountConsumer />,
-          <ConfigurationConsumer />,
-          <FundManagerConsumer />,
-          <SetupConsumer />,
-        ]}
-      >
-        {([account, configuration, manager, setup]) => (
-          <Fragment>
-            <SetupTransactions
-              progress={
-                (setup.setupBegin && !!this.state.values) ||
-                setup.setupInProgress ||
-                setup.setupComplete
-              }
-              values={this.state.values}
-              update={manager.update}
-              updateSetup={setup.update}
-              routes={manager.routes}
-              fund={manager.fund}
-              setup={setup}
-            />
-
-            <PoliciesTransactions
-              progress={
-                !R.isEmpty(R.path(['state', 'values', 'policies'], this)) &&
-                setup.isComplete &&
-                !!manager.fund
-              }
-              values={this.state.values}
-              fund={manager.fund}
-            />
-
-            {(!!this.state.values || !manager.fund) && (
-              <TokensQuery>
-                {queryProps => (
-                  <SetupFormContainer
-                    {...this.props}
-                    account={account}
-                    configuration={configuration}
-                    page={this.state.page}
-                    setPage={this.setPage}
-                    steps={this.state.steps}
-                    setShowModal={this.setShowModal}
-                    setFundValues={this.setFundValues}
-                    showModal={this.state.showModal}
-                    validateOnBlur={true}
-                    validateOnChange={false}
-                    tokens={R.path(['data', 'tokens'], queryProps)}
-                  />
-                )}
-              </TokensQuery>
-            )}
-          </Fragment>
-        )}
-      </Composer>
-    );
-  }
-}
+          {(!!fundValues || !manager.fund) && (
+            <TokensQuery>
+              {queryProps => (
+                <SetupFormContainer
+                  {...props}
+                  account={account}
+                  configuration={configuration}
+                  page={page}
+                  setPage={setPage}
+                  steps={steps}
+                  setShowModal={setShowModal}
+                  setFundValues={setFundValues}
+                  showModal={showModal}
+                  validateOnBlur={true}
+                  validateOnChange={false}
+                  tokens={R.path(['data', 'tokens'], queryProps)}
+                />
+              )}
+            </TokensQuery>
+          )}
+        </Fragment>
+      )}
+    </Composer>
+  );
+};
 
 export default compose(
   withRouter,
