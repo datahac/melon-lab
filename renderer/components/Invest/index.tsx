69de99845328476778f254d682de3b320a1af787
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-02-05 13:58:09 +0100

    Refactor invest and redeem

--- renderer/components/Invest/index.tsx
+++ renderer/components/Invest/index.tsx
@@ -1,6 +1,5 @@
 import * as Tm from '@melonproject/token-math';
-
-import React, { Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import Participation from '~/components/ParticipationForm';
 import InvestTransactions from '+/components/InvestTransactions';
 import withForm from './withForm';
@@ -19,90 +18,69 @@ const ParticipationFormContainer = withForm(props => (
   />
 ));
 
-class InvestContainer extends React.PureComponent {
-  state = {
-    values: null,
-    step: null,
-  };
+const InvestContainer = ({ address, ...props }) => {
+  const [step, setStep] = useState(null);
+  const [investValues, setInvestValues] = useState(null);
 
-  setInvestValues = values => {
-    this.setState({
-      values,
-      step: 0,
-    });
-  };
+  return (
+    <Composer
+      components={[
+        <AccountConsumer />,
+        <FundQuery address={address} />,
+        ({ results: [account], render }) => (
+          <RequestQuery
+            fundAddress={address}
+            userAddress={account}
+            children={render}
+          />
+        ),
+      ]}
+    >
+      {([account, fundProps, requestProps]) => {
+        const waitingTime = R.pathOr(
+          '0',
+          ['data', 'hasActiveRequest', 'waitingTime'],
+          requestProps,
+        );
+        const hasActiveRequest = R.path(
+          ['data', 'hasActiveRequest'],
+          requestProps,
+        );
 
-  setStep = step => {
-    this.setState({
-      step,
-    });
-  };
+        const isWaiting = Tm.greaterThan(waitingTime, '0');
+        const readyToExecute = Tm.isZero(waitingTime) && hasActiveRequest;
+        const isInitialRequest = Tm.isZero(
+          R.pathOr('0', ['data', 'fund', 'totalSupply', 'quantity'], fundProps),
+        );
 
-  render() {
-    return (
-      <Composer
-        components={[
-          <AccountConsumer />,
-          <FundQuery address={this.props.address} />,
-          ({ results: [account], render }) => (
-            <RequestQuery
-              fundAddress={this.props.address}
-              userAddress={account}
-              children={render}
+        return (
+          <Fragment>
+            <ParticipationFormContainer
+              {...props}
+              setInvestValues={setInvestValues}
+              loading={R.path(['loading'], fundProps)}
+              sharePrice={R.path(['data', 'fund', 'sharePrice'], fundProps)}
+              setStep={setStep}
+              isWaiting={isWaiting}
+              readyToExecute={readyToExecute}
+              executeRequest={() => setStep(3)}
             />
-          ),
-        ]}
-      >
-        {([account, fundProps, requestProps]) => {
-          const waitingTime = R.pathOr(
-            '0',
-            ['data', 'hasActiveRequest', 'waitingTime'],
-            requestProps,
-          );
-          const hasActiveRequest = R.path(
-            ['data', 'hasActiveRequest'],
-            requestProps,
-          );
 
-          const isWaiting = Tm.greaterThan(waitingTime, '0');
-          const readyToExecute = Tm.isZero(waitingTime) && hasActiveRequest;
-          const isInitialRequest = Tm.isZero(
-            R.pathOr(
-              '0',
-              ['data', 'fund', 'totalSupply', 'quantity'],
-              fundProps,
-            ),
-          );
-
-          return (
-            <Fragment>
-              <ParticipationFormContainer
-                {...this.props}
-                setInvestValues={this.setInvestValues}
-                loading={R.path(['loading'], fundProps)}
-                sharePrice={R.path(['data', 'fund', 'sharePrice'], fundProps)}
-                setStep={this.setStep}
-                isWaiting={isWaiting}
-                readyToExecute={readyToExecute}
-                executeRequest={() => this.setStep(3)}
-              />
-
-              <InvestTransactions
-                fundAddress={this.props.address}
-                values={this.state.values}
-                setStep={this.setStep}
-                setInvestValues={this.setInvestValues}
-                step={this.state.step}
-                isWaiting={isWaiting}
-                readyToExecute={readyToExecute}
-                isInitialRequest={isInitialRequest}
-              />
-            </Fragment>
-          );
-        }}
-      </Composer>
-    );
-  }
-}
+            <InvestTransactions
+              fundAddress={address}
+              values={investValues}
+              setStep={setStep}
+              setInvestValues={setInvestValues}
+              step={step}
+              isWaiting={isWaiting}
+              readyToExecute={readyToExecute}
+              isInitialRequest={isInitialRequest}
+            />
+          </Fragment>
+        );
+      }}
+    </Composer>
+  );
+};
 
 export default withRouter(InvestContainer);
--- renderer/components/Redeem/index.tsx
+++ renderer/components/Redeem/index.tsx
@@ -1,55 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import RedeemForm from '~/components/ReedemForm';
 import withForm from './withForm';
 import { withRouter } from 'next/router';
 import { FundQuery } from './data/fund';
 import * as R from 'ramda';
-import Composer from 'react-composer';
-import { AccountConsumer } from '+/components/AccountContext';
 import RedeemTransactions from '+/components/RedeemTransactions';
 
 const RedeemFormContainer = withForm(props => <RedeemForm {...props} />);
 
-class RedeemContainer extends React.PureComponent {
-  state = {
-    values: null,
-  };
+const RedeemContainer = ({ address, ...props }) => {
+  const [redeemValues, setRedeemValues] = useState(null);
 
-  setRedeemValues = values => {
-    this.setState({
-      values,
-    });
-  };
+  return (
+    <FundQuery address={address}>
+      {fundProps => {
+        return (
+          <Fragment>
+            <RedeemFormContainer
+              {...props}
+              loading={R.path(['loading'], fundProps)}
+              setRedeemValues={setRedeemValues}
+              sharePrice={R.path(['data', 'fund', 'sharePrice'], fundProps)}
+            />
 
-  render() {
-    return (
-      <Composer
-        components={[
-          <AccountConsumer />,
-          <FundQuery address={this.props.address} />,
-        ]}
-      >
-        {([account, fundProps]) => {
-          return (
-            <Fragment>
-              <RedeemFormContainer
-                {...this.props}
-                loading={R.path(['loading'], fundProps)}
-                setRedeemValues={this.setRedeemValues}
-                sharePrice={R.path(['data', 'fund', 'sharePrice'], fundProps)}
-              />
-
-              <RedeemTransactions
-                fundAddress={this.props.address}
-                values={this.state.values}
-                setRedeemValues={this.setRedeemValues}
-              />
-            </Fragment>
-          );
-        }}
-      </Composer>
-    );
-  }
-}
+            <RedeemTransactions
+              fundAddress={address}
+              values={redeemValues}
+              setRedeemValues={setRedeemValues}
+            />
+          </Fragment>
+        );
+      }}
+    </FundQuery>
+  );
+};
 
 export default withRouter(RedeemContainer);
