3a4e41c1a7f04b11009f9a17525ddf6ad7945330
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-02-05 17:00:03 +0100

    Refactor open orders component

--- renderer/components/OpenOrders/index.tsx
+++ renderer/components/OpenOrders/index.tsx
@@ -1,34 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OpenOrders from '~/components/OpenOrders';
 import { OpenOrdersQuery } from './data/openOrders';
 import * as R from 'ramda';
 import CancelOrder from '+/components/CancelOrder';
 
-export default class OpenOrdersContainer extends React.Component {
-  state = { selectedOrder: null };
+const OpenOrdersContainer = ({ address, canInteract, isManager }) => {
+  const [selectedOrder, setSelectedOrder] = useState(null);
 
-  setSelectedOrder = selectedOrder => {
-    this.setState({ selectedOrder });
-  };
+  return (
+    <OpenOrdersQuery fundAddress={address}>
+      {queryProps => (
+        <React.Fragment>
+          <OpenOrders
+            canInteract={canInteract}
+            isManager={isManager}
+            orders={R.pathOr([], ['data', 'openOrders'], queryProps)}
+            onClick={setSelectedOrder}
+          />
+          <CancelOrder
+            values={selectedOrder}
+            setSelectedOrder={setSelectedOrder}
+          />
+        </React.Fragment>
+      )}
+    </OpenOrdersQuery>
+  );
+};
 
-  render() {
-    return (
-      <OpenOrdersQuery fundAddress={this.props.address}>
-        {queryProps => (
-          <React.Fragment>
-            <OpenOrders
-              canInteract={this.props.canInteract}
-              isManager={this.props.isManager}
-              orders={R.pathOr([], ['data', 'openOrders'], queryProps)}
-              onClick={this.setSelectedOrder}
-            />
-            <CancelOrder
-              values={this.state.selectedOrder}
-              setSelectedOrder={this.setSelectedOrder}
-            />
-          </React.Fragment>
-        )}
-      </OpenOrdersQuery>
-    );
-  }
-}
+export default OpenOrdersContainer;
