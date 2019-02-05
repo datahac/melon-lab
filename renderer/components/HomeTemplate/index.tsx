379423a89f0294c331c0466c80110da0e4cc3fa9
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-02-05 16:44:38 +0100

    Refactor components

--- renderer/components/HomeTemplate/index.tsx
+++ renderer/components/HomeTemplate/index.tsx
@@ -9,56 +9,52 @@ import { ConfigurationConsumer } from '+/components/ConfigurationContext';
 import GetStarted from '+/components/GetStarted';
 import Ranking from '+/components/Ranking';
 
-export default class HomeTemplateContainer extends React.PureComponent {
-  render() {
-    return (
-      <Composer
-        components={[
-          <AccountConsumer />,
-          <BalanceConsumer />,
-          <NetworkConsumer />,
-          <CapabilityConsumer />,
-          <ConfigurationConsumer />,
-        ]}
-      >
-        {([account, balances, network, capabibility, configuration]) => {
-          const { title, text, icon, children } = this.props;
+const HomeTemplateContainer = ({ title, text, icon, children }) => (
+  <Composer
+    components={[
+      <AccountConsumer />,
+      <BalanceConsumer />,
+      <NetworkConsumer />,
+      <CapabilityConsumer />,
+      <ConfigurationConsumer />,
+    ]}
+  >
+    {([account, balances, network, capabibility, configuration]) => {
+      return (
+        <HomeTemplate
+          HeaderProps={{
+            address: account,
+            ethBalance: balances && balances.eth,
+            canInvest: capabibility && capabibility.canInvest,
+            canInteract: capabibility && capabibility.canInteract,
+            canonicalPriceFeedAddress:
+              configuration && configuration.canonicalPriceFeedAddress,
+            network: network && network.network,
+            currentBlock: network && network.currentBlock,
+            blockOverdue: network && network.blockOverdue,
+            nodeSynced: network && network.nodeSynced,
+            priceFeedUp: network && network.priceFeedUp,
+          }}
+          HeadlineProps={
+            title
+              ? {
+                  title,
+                  text,
+                  icon,
+                }
+              : null
+          }
+          GetStarted={GetStarted}
+          GetStartedProps={{
+            isHome: true,
+          }}
+          Ranking={Ranking}
+        >
+          {children}
+        </HomeTemplate>
+      );
+    }}
+  </Composer>
+);
 
-          return (
-            <HomeTemplate
-              HeaderProps={{
-                address: account,
-                ethBalance: balances && balances.eth,
-                canInvest: capabibility && capabibility.canInvest,
-                canInteract: capabibility && capabibility.canInteract,
-                canonicalPriceFeedAddress:
-                  configuration && configuration.canonicalPriceFeedAddress,
-                network: network && network.network,
-                currentBlock: network && network.currentBlock,
-                blockOverdue: network && network.blockOverdue,
-                nodeSynced: network && network.nodeSynced,
-                priceFeedUp: network && network.priceFeedUp,
-              }}
-              HeadlineProps={
-                title
-                  ? {
-                      title,
-                      text,
-                      icon,
-                    }
-                  : null
-              }
-              GetStarted={GetStarted}
-              GetStartedProps={{
-                isHome: true,
-              }}
-              Ranking={Ranking}
-            >
-              {children}
-            </HomeTemplate>
-          );
-        }}
-      </Composer>
-    );
-  }
-}
+export default HomeTemplateContainer;
--- renderer/components/RecentTrades/index.tsx
+++ renderer/components/RecentTrades/index.tsx
@@ -2,28 +2,30 @@ import React from 'react';
 import RecentTrades from '~/components/RecentTrades';
 import RecentTradesQuery from './data/recentTrades';
 
-export default class RecentTradesContainer extends React.PureComponent {
-  render() {
-    return (
-      <RecentTradesQuery
-        fundAddress={this.props.fundAddress}
-        baseAsset={this.props.baseAsset}
-        quoteAsset={this.props.quoteAsset}
-      >
-        {recentTradesProps => {
-          return (
-            <RecentTrades
-              {...this.props}
-              trades={
-                (recentTradesProps.data &&
-                  recentTradesProps.data.recentTrades) ||
-                []
-              }
-              loading={recentTradesProps.loading}
-            />
-          );
-        }}
-      </RecentTradesQuery>
-    );
-  }
-}
+const RecentTradesContainer = ({
+  fundAddress,
+  baseAsset,
+  quoteAsset,
+  ...props
+}) => (
+  <RecentTradesQuery
+    fundAddress={fundAddress}
+    baseAsset={baseAsset}
+    quoteAsset={quoteAsset}
+  >
+    {recentTradesProps => {
+      return (
+        <RecentTrades
+          {...props}
+          trades={
+            (recentTradesProps.data && recentTradesProps.data.recentTrades) ||
+            []
+          }
+          loading={recentTradesProps.loading}
+        />
+      );
+    }}
+  </RecentTradesQuery>
+);
+
+export default RecentTradesContainer;
