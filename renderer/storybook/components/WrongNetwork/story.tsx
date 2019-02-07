e68d8cf774ba93e0978f8241e75c2f2413fce020
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-02-07 20:53:01 +0100

    Cleanup

--- renderer/storybook/components/WrongNetwork/story.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import WrongNetwork from './index';
-
-storiesOf('Components|Wrong Network', module).add('Default', () => {
-  return <WrongNetwork />;
-});
