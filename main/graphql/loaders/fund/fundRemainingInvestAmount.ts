5c026884841b75050c8add980e8ffecf3cb04c29
Sebastian Siemssen <fubhy@fubhy.com>
2019-07-22 12:46:25 +0200

    Parse int value from .env parameter.

--- main/graphql/loaders/fund/fundRemainingInvestAmount.ts
+++ main/graphql/loaders/fund/fundRemainingInvestAmount.ts
@@ -9,10 +9,10 @@ import {
 function fundRemainingInvestAmount(daiPrice, investmentAssetPrice, fundGav) {
   const maxCap = createQuantity(
     daiPrice.base.token,
-    process.env.MAX_INVEST_AMOUNT || 10000,
+    parseInt(process.env.MAX_INVEST_AMOUNT || '10000', 10),
   );
-  const maxCapInWeth = valueIn(daiPrice, maxCap);
 
+  const maxCapInWeth = valueIn(daiPrice, maxCap);
   if (greaterThan(maxCapInWeth, fundGav)) {
     const remaining = subtract(maxCapInWeth, fundGav);
     return valueIn(investmentAssetPrice, remaining);
